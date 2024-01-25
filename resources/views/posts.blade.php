ac09f462bd74de3453d59b809e57c08b42639c22
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-25 19:46:42 -0300

    Properly displaying html tags
    Section 4

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
     <article>
         <h1>
             <a href="/post/{{ $post->id }}">
-                {{ $post->title }}
+                {!! $post->title !!}
             </a>
         </h1>
         <div>
