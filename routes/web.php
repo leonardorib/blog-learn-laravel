b68ad63aead4f4937eef7c16e7f98520bb6ace73
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-25 21:15:48 -0300

    Added a category route
    Section 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
@@ -23,5 +24,12 @@ Route::get('/', function () {
 Route::get('post/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
