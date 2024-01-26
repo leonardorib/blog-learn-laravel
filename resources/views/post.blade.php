1d11c76ec0cf2817b5ad07d5ab89d6e06bd85478
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-25 21:05:00 -0300

    Added a relationship with Eloquent
    Section 4

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,12 @@
         <h1>
             {!! $post->title !!}
         </h1>
-        {!! $post->body !!}
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
+        <div>
+            {!! $post->body !!}
+        </div>
     </article>
     <a href="/">Go Back</a>
 </x-layout>
