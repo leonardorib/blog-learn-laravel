41e755866cc300e9cf03fd2737809881975d260a
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-26 15:02:10 -0300

    Added pagination
    Section 8

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -6,11 +6,13 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="request()->routeIs('home') && is_null(request()->query('category'))">
+    <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+        :active="request()->routeIs('home') && is_null(request()->query('category'))">
         All
     </x-dropdown-item>
     @foreach ($categories as $category)
-    <x-dropdown-item href="?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+    <x-dropdown-item
+        href="?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
         :active="request()->query('category') === $category->slug">
         {{ ucwords($category->name) }}
     </x-dropdown-item>
