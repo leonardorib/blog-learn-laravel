dc27f55e24a26892a26ab356fe3d976722408ee9
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-27 17:24:44 -0300

    Added comment component
    Section 10

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" class="bg-gray-200" width="60" height="60" alt="" >
+    </div>
+
+    <div>
+        <header>
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sequi atque libero ut impedit deserunt. Nostrum, eaque odio. Nobis expedita, harum sunt ratione nulla unde recusandae? Necessitatibus soluta ad voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quos cupiditate corrupti, non officia optio omnis harum, aperiam at illo repellendus eaque, fuga ducimus debitis nemo nulla laudantium? Numquam, explicabo.
+        </p>
+    </div>
+</article>
