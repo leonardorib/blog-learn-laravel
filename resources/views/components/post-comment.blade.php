5ac3c330b62a738ae341ad22ee9aa1c0d89f244e
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-27 18:24:19 -0300

    Designed comment form
    Section 10

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,20 +1,28 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" class="bg-gray-200" width="60" height="60" alt="" >
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img
+                src="https://i.pravatar.cc/100?u={{ $comment->id }}"
+                class="bg-gray-200"
+                width="60"
+                height="60"
+                alt=""
+            >
+        </div>
 
-    <div>
-        <header>
-            <h3 class="font-bold">{{ $comment->author->name }}</h3>
-            <p class="text-xs">
-                Posted <time>{{ $comment->created_at->diffForHumans() }}</time>
+        <div>
+            <header>
+                <h3 class="font-bold">{{ $comment->author->name }}</h3>
+                <p class="text-xs">
+                    Posted <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -74,13 +74,51 @@
                     </div>
                 </div>
 
-                @if ($post->comments->count())
-                    <section class="col-span-8 col-start-5 mt-10 space-y-4">
-                        @foreach ($post->comments as $comment)
-                            <x-post-comment :comment="$comment" />
-                        @endforeach
-                    </section>
-                @endif
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-4">
+                    <x-panel>
+                        <form
+                            method="POST"
+                            action="#"
+                        >
+                            @csrf
+
+                            <header class="flex items-center">
+                                <img
+                                    src="https://i.pravatar.cc/100?u={{ auth()->id() }}"
+                                    class="bg-gray-200"
+                                    width="40"
+                                    height="40"
+                                    alt=""
+                                >
+
+                                <h2 class="ml-3">Want to participate?</h2>
+                            </header>
+
+                            <div class="mt-6">
+                                <textarea
+                                    name="body"
+                                    class="w-full text-sm focus:outline-none focus:ring p-1"
+                                    rows="5"
+                                    placeholder="Quick, thing of something to say!"
+                                ></textarea>
+                            </div>
+
+                            <div class="flex justify-end mt-4 pt-4 border-t border-gray-200">
+                                <button
+                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600 transition ease-in-out duration-150"
+                                    type="submit"
+                                >Post</button>
+                            </div>
+                        </form>
+                    </x-panel>
+
+                    @foreach ($post->comments as $comment)
+                        <x-post-comment :comment="$comment" />
+                    @endforeach
+                </section>
+
+
             </article>
         </main>
     </section>
