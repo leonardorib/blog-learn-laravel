93861f2c09356c3ea3bca9e088d993a6fc597100
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-27 18:40:45 -0300

    Date formatting

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -16,7 +16,7 @@
             <header>
                 <h3 class="font-bold">{{ $comment->author->name }}</h3>
                 <p class="text-xs">
-                    Posted <time>{{ $comment->created_at->diffForHumans() }}</time>
+                    Posted <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time>
                 </p>
             </header>
 
