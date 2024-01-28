85054ef413ed9b72253e7acfc6d36a05ddbc52f7
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-27 21:11:09 -0300

    Defined a generic Newsletter interface
    Section 11

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\Newsletter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = new \MailchimpMarketing\ApiClient();
+
+            $client->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => config('services.mailchimp.server'),
+            ]);
+
+            return new \App\Services\MailchimpNewsletter($client);
+        });
     }
 
     /**
--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -2,25 +2,7 @@
 
 namespace App\Services;
 
-use MailchimpMarketing\ApiClient;
-
-class Newsletter
+interface Newsletter
 {
-    public function subscribe(string $email, $list = null)
-    {
-        $list ??= config('services.mailchimp.lists.subscribers');
-
-        return $this->client()->lists->addListMember($list, [
-            'email_address' => $email,
-            'status' => 'subscribed',
-        ]);
-    }
-
-    protected function client()
-    {
-        return (new ApiClient())->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => config('services.mailchimp.server'),
-        ]);
-    }
+    public function subscribe(string $email, $list = null);
 }
