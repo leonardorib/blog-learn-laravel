770dce90b38ec56f5a96ebbc82a545b29f3f4be7
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-27 21:26:24 -0300

    Added an admin only post creation page barebones
    Section 12

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,13 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('posts.show', [
-            'post' => $post
+            'post' => $post,
         ]);
+    }
+
+    public function create()
+    {
+
+        return view('posts.create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('newsletter', NewsletterController::class);
+
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
