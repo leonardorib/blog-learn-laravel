df0d9c8b56ec81fa316dbb07dbacfda0e39b23fd
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-27 21:56:18 -0300

    Added post creation feature
    Section 12

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
@@ -24,5 +25,22 @@ class PostController extends Controller
     {
 
         return view('posts.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => ['required', 'min:2', 'max:255'],
+            'slug' => ['required', 'min:2', 'max:255', Rule::unique('posts', 'slug')],
+            'excerpt' => ['required', 'min:2', 'max:255'],
+            'body' => ['required', 'min:2', 'max:4098'],
+            'category_id' => ['required', Rule::exists('categories', 'id')],
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+
+        Post::create($attributes);
+
+        return redirect('/');
     }
 }
